bd7afa1b7836dfbd5a4486d2e2947e7ac27d3c0e
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-06-12 22:57:31 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, driver){
+ let newArray = drivers.filter( val => driver === val || driver.toLowerCase() === val)  
+ return newArray
+ }
+function fuzzyMatch(drivers , firstTwo){
+    let newArray1 = drivers.filter(res => res.slice(0,2) === firstTwo)
+  return newArray1
+}
+function matchName(drivers, driver){
+   let obj = drivers.filter(res1 => res1.name=== driver)
+   return obj
+}
